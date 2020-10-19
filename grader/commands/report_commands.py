0992be62e036697c01eb79597d10d0b2c2204449
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2020-10-19 09:50:25 -0400

    adding the commands section which will hold all CLI commands for click. will not do much of the functionality, that will be supported in other modules

---
+++ grader/commands/report_commands.py
@@ -0,0 +1,70 @@
+"""
+    Purpose:
+        Report Command Group is responsible for generating reports
+"""
+
+# Python Library Imports
+import click
+import getpass
+
+# Local Python Library Imports
+from grader.report.pygrade_report import PyGradeReport
+
+
+###
+# Report Commands
+###
+
+
+@click.command("generate")
+@click.option(
+    "--source",
+    "source_code",
+    required=True,
+    default=None,
+    type=str,
+    help="Source Code to Grade",
+)
+@click.option(
+    "--report",
+    "report_name",
+    required=True,
+    default=None,
+    type=str,
+    help="Name of the Report",
+)
+@click.option(
+    "--candidate",
+    "candidate_name",
+    required=True,
+    default=None,
+    type=str,
+    help="Name of the Candidate",
+)
+@click.option(
+    "--overwrite",
+    flag_value=True,
+    type=bool,
+    default=False,
+    help="Overwrite report if it already exists?",
+)
+@click.pass_context
+def generate(
+    cli_context: object,
+    report_name: str,
+    candidate_name: str,
+    source_code: str,
+    overwrite: bool,
+) -> None:
+    """
+    Generate a pygrade report
+    """
+    click.echo("Generating Report")
+
+    # Build base report object
+    pygrade_report = PyGradeReport(report_name, candidate_name, source_code)
+
+    # Generate the report
+    pygrade_report.generate_report(overwrite=overwrite)
+
+    click.echo(f"Report Created: {pygrade_report.report_path}")
